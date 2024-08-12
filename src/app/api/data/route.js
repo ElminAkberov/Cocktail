8482846f0aa54ebde4a729db5140ed0088c13eb2
ElminAkberov <ekberovelmin04@gmail.com>
2024-08-12 16:22:30 +0400

    Cocktail

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -9,7 +9,6 @@ export async function GET() {
         }
         const data = await res.json();
         return NextResponse.json(data)
-
     }
     catch (error) {
         return NextResponse.json({ error: 'Failed to fetch data' });
